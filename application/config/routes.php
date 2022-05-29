c826f0140ce3fd200614fc8926ab6950a479a69b
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-05-29 23:30:23 +0700

    Add custom routes and prepare mc files

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,72 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'auth';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+/**
+ * CUSTOM ROUTING
+ */ 
+
+# BASIC
+$route['reg_user'] = 'auth/reg_user';
+$route['reg_mitra'] = 'auth/reg_mitra';
+$route['login_user'] = 'auth/login_user';
+$route['login_mitra'] = 'auth/login_mitra';
+$route['profil_user'] = 'auth/profil_user';
+$route['profil_mitra'] = 'auth/profil_mitra';
+$route['edit_profil_user'] = 'auth/edit_profil_user';
+$route['edit_profil_mitra'] = 'auth/edit_profil_mitra';
+$route['edit_pass_user'] = 'auth/edit_pass_user';
+$route['edit_pass_mitra'] = 'auth/edit_pass_mitra';
+$route['logout'] = 'auth/logout';
+
+# USER
+// dashboard
+$route['dashboard'] = 'user/dashboard';
+// admin
+$route['del_admin'] = 'actor/del_admin';
+$route['active_admin'] = 'actor/active_admin';
+// mitra
+$route['del_mitra'] = 'actor/del_mitra';
+$route['active_mitra'] = 'actor/active_mitra';
+// sampah
+$route['add_sampah'] = 'user/add_sampah';
+$route['edit_sampah'] = 'user/edit_sampah';
+$route['del_sampah'] = 'user/del_sampah';
+// edukasi
+$route['add_edukasi'] = 'user/add_edukasi';
+$route['edit_edukasi'] = 'user/edit_edukasi';
+$route['del_edukasi'] = 'user/del_edukasi';
+// penjualan
+$route['add_penjualan'] = 'trans/add_penjualan';
+$route['del_penjualan'] = 'trans/del_penjualan';
+$route['confirm_jual'] = 'trans/confirm_jual';
+// pembeli
+$route['add_pembeli'] = 'actor/add_pembeli';
+$route['edit_pembeli'] = 'actor/edit_pembeli';
+$route['del_pembeli'] = 'actor/del_pembeli';
+// pembayaran
+$route['confirm_bayar'] = 'trans/confirm_bayar';
+// penjemputan
+$route['confirm_jemput'] = 'trans/confirm_jemput';
+// all data
+$route['data_setoran'] = 'user/data_setoran';
+$route['data_penjemputan'] = 'user/data_penjemputan';
+$route['data_pembayaran'] = 'user/data_pembayaran';
+$route['data_penjualan'] = 'user/data_penjualan';
+$route['data_pembeli'] = 'user/data_pembeli';
+$route['data_sampah'] = 'user/data_sampah';
+
+
+
+# MITRA
+// rekening
+$route['edit_rek'] = 'mitra/edit_rek';
+// struktur
+$route['edit_struktur'] = 'mitra/edit_struktur';
+// edukasi
+$route['join_edukasi'] = 'mitra/join_edukasi';
+// setoran
+$route['add_setoran'] = 'mitra/add_setoran';
+$route['edit_setoran'] = 'mitra/edit_setoran';
+$route['del_setoran'] = 'mitra/del_setoran';
