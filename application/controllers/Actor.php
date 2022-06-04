d77ea85d2604e4b49a7c26b709c19867822df58c
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-06-04 19:41:17 +0700

    Add module ON-OFF mitra and few changes alert msg

--- application/controllers/Actor.php
+++ application/controllers/Actor.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Actor extends CI_Controller
 {
@@ -27,10 +27,10 @@ class Actor extends CI_Controller
 			'user' => $user,
 			'data_admin' => $this->acmodel->get_data_admin()
 		];
-		
+
 		$this->load->view('sections/main', $data);
 	}
-	
+
 	public function on_admin()
 	{
 		$id_admin = $this->input->post('id_admin');
@@ -45,11 +45,11 @@ class Actor extends CI_Controller
 		$this->session->set_flashdata(
 			'message',
 			'<div class="alert alert-success alert-dismissible fade show" role="alert">
-				Berhasil mengaktifkan admin ' . $admin['email'] . '
+				Akun admin <i>' . $admin['email'] . '</i> telah <span class="badge bg-primary">AKTIF</span>
 				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 			</div>'
 		);
-		
+
 		redirect('data_admin');
 	}
 
@@ -67,11 +67,71 @@ class Actor extends CI_Controller
 		$this->session->set_flashdata(
 			'message',
 			'<div class="alert alert-success alert-dismissible fade show" role="alert">
-				Berhasil menonaktifkan admin ' . $admin['email'] . '
+				Akun admin <i>' . $admin['email'] . '</i> telah <span class="badge bg-danger">NONAKTIF</span>
 				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 			</div>'
 		);
-		
+
 		redirect('data_admin');
+	}
+
+	public function data_mitra()
+	{
+		$email_sess = $this->session->userdata('email');
+		$user = $this->db->get_where('user', ['email' => $email_sess])->row_array();
+		$mitra = $this->db->get_where('mitra', ['email' => $email_sess])->row_array();
+		$data = [
+			'project' => 'Bank Sampah Induk Rumah Harum',
+			'title' => 'Data Mitra',
+			'mitra' => $mitra,
+			'user' => $user,
+			'data_mitra' => $this->acmodel->get_data_mitra()
+		];
+
+		$this->load->view('sections/main', $data);
+	}
+
+	public function on_mitra()
+	{
+		$id_mitra = $this->input->post('id_mitra');
+		$mitra = $this->db->get_where('mitra', ['id_mitra' => $id_mitra])->row_array();
+
+		// var_dump($id_mitra);
+		// die;
+
+		# Passing $input as a parameter of createmitra() function to execute adding data to database
+		$this->acmodel->active_mitra($id_mitra);
+		# Add an alert message to session if createmitra() process is successful
+		$this->session->set_flashdata(
+			'message',
+			'<div class="alert alert-success alert-dismissible fade show" role="alert">
+				Akun mitra <i>' . $mitra['email'] . '</i> telah <span class="badge bg-primary">AKTIF</span>
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>'
+		);
+
+		redirect('data_mitra');
+	}
+
+	public function off_mitra()
+	{
+		$id_mitra = $this->input->post('id_mitra');
+		$mitra = $this->db->get_where('user', ['id_user' => $id_mitra])->row_array();
+
+		// var_dump($id_mitra);
+		// die;
+
+		# Passing $input as a parameter of createUser() function to execute adding data to database
+		$this->acmodel->inactive_mitra($id_mitra);
+		# Add an alert message to session if createUser() process is successful
+		$this->session->set_flashdata(
+			'message',
+			'<div class="alert alert-success alert-dismissible fade show" role="alert">
+				Akun mitra <i>' . $mitra['email'] . '</i> telah <span class="badge bg-danger">NONAKTIF</span>
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>'
+		);
+
+		redirect('data_mitra');
 	}
 }
