533cd40c5fd90c31c567ad9be078a317962268e6
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-06-04 21:47:06 +0700

    Add logic Auth for status user and mitra

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -44,24 +44,38 @@ class Auth extends CI_Controller
 
 		# IF statement to check if user data exists
 		if ($mitra) {
-			# IF statement to check whether entered password matches user data
-			if (password_verify($data['password'], $mitra['password'])) {
-				# $data variable to save field email & username from $mitra
-				$data = [
-					'email' => $mitra['email'],
-					'username' => $mitra['username'],
-					'tipe' => $mitra['tipe_mitra']
-				];
-				# Add $data values to session
-				$this->session->set_userdata($data);
-
-				redirect('dashboard');
+			if ($mitra['status'] == 1) {
+				# IF statement to check whether entered password matches user data
+				if (password_verify($data['password'], $mitra['password'])) {
+					# $data variable to save field email & username from $mitra
+					$data = [
+						'email' => $mitra['email'],
+						'username' => $mitra['username'],
+						'tipe' => $mitra['tipe_mitra']
+					];
+					# Add $data values to session
+					$this->session->set_userdata($data);
+	
+					redirect('dashboard');
+				} else {
+					# If password is not matches, will be send wrong password message
+					$this->session->set_flashdata(
+						'message',
+						'<div class="alert alert-danger alert-dismissible fade show" role="alert">
+							Maaf password anda salah, coba lagi!
+							<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+						</div>'
+					);
+					# It will be returned to login page
+					redirect('login_mitra');
+				}
 			} else {
 				# If password is not matches, will be send wrong password message
 				$this->session->set_flashdata(
 					'message',
 					'<div class="alert alert-danger alert-dismissible fade show" role="alert">
-						Maaf password anda salah, coba lagi!
+						Maaf akun anda telah di <span class="badge bg-danger">Non Aktif</span> kan,
+						silahkan hubungi pihak Bank Sampah Induk Rumah Harum untuk proses pengaktifan kembali!
 						<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 					</div>'
 				);
@@ -103,24 +117,38 @@ class Auth extends CI_Controller
 
 		# IF statement to check if user data exists
 		if ($user) {
-			# IF statement to check whether entered password matches user data
-			if (password_verify($data['password'], $user['password'])) {
-				# $data variable to save field email & username from $user
-				$data = [
-					'email' => $user['email'],
-					'username' => $user['username'],
-					'role' => $user['role']
-				];
-				# Add $data values to session
-				$this->session->set_userdata($data);
-
-				redirect('dashboard_user');
+			if ($user['status'] == 1) {
+				# IF statement to check whether entered password matches user data
+				if (password_verify($data['password'], $user['password'])) {
+					# $data variable to save field email & username from $user
+					$data = [
+						'email' => $user['email'],
+						'username' => $user['username'],
+						'role' => $user['role']
+					];
+					# Add $data values to session
+					$this->session->set_userdata($data);
+	
+					redirect('dashboard_user');
+				} else {
+					# If password is not matches, will be send wrong password message
+					$this->session->set_flashdata(
+						'message',
+						'<div class="alert alert-danger alert-dismissible fade show" role="alert">
+							Maaf password anda salah, coba lagi!
+							<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+						</div>'
+					);
+					# It will be returned to login page
+					redirect('login_user');
+				}
 			} else {
 				# If password is not matches, will be send wrong password message
 				$this->session->set_flashdata(
 					'message',
 					'<div class="alert alert-danger alert-dismissible fade show" role="alert">
-						Maaf password anda salah, coba lagi!
+						Maaf akun anda telah di <span class="badge bg-danger">Non Aktif</span> kan,
+						silahkan hubungi ketua Bank Sampah Induk Rumah Harum untuk proses pengaktifan kembali!
 						<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 					</div>'
 				);
